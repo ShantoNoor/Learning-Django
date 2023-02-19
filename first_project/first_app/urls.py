ff08a16b2b24fa13924195040be78a175d7cc1a4
Shanto Noor <shantonoor900@gmail.com>
2023-02-19 19:03:31 +0600

    Coding

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
 from first_app import views
 
+app_name = 'first_app'
+
 urlpatterns = [
-    path('', views.index),
-    re_path(r'^index2$', views.index2),
-    path('form/', views.form_input),
-    path('webform/', views.web_form),
+    path('', views.index, name='home'),
+    re_path(r'^index2$', views.index2, name='index2'),
+    path('form/', views.form_input, name='form'),
+    path('webform/', views.web_form, name='webform'),
 ]
