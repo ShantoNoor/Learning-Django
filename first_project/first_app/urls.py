ba15a4d0bd2d756673490c87c7f50693d95a9764
Shanto Noor <shantonoor900@gmail.com>
2023-02-19 08:54:12 +0600

    Coding

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from first_app import views
 
 urlpatterns = [
+    path('', views.index),
     re_path(r'^index2$', views.index2),
-    re_path(r'', views.index)
+    path('form/', views.form_input),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import AccessRecord
+from . import forms
+
 # Create your views here.
 
 def index(request):
@@ -12,3 +14,22 @@ def index(request):
 def index2(request):
     my_dict = {'insert_me': 'heoool from vies.py'}
     return render(request, 'first_app/index2.html', context=my_dict)
+
+
+def form_input(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            text = form.cleaned_data['text']
+
+            print(f'Name: {name}')
+            print(f'Email: {email}')
+            print(f'Text: {text}')
+
+    return render(request, 'first_app/form.html', {'form': form})
+
